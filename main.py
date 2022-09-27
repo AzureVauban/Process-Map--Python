68439b857822c5c256b12f9f25feeab501565ae9
Azurevauban <azurewight@gmail.com>
2022-09-27 17:56:56 -0500

    commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class NodeB:
     """
     ingredient: str = ''
     amountonhand: int = 0
-    amountneededpercraft: int = 0
+    amountneeded: int = 0
     amountmadepercraft: int = 0
     amountresulted: int = 0
     queueamountresulted: dict = {}  # use this to test the math function
@@ -29,7 +29,7 @@ class NodeB:
         """
         self.amountonhand = red
         self.amountmadepercraft = blue
-        self.amountneededpercraft = yellow
+        self.amountneeded = yellow
         self.queueamountresulted = {}
         self.ingredient = name
         self.amountresulted = 0
@@ -57,22 +57,18 @@ class Node(NodeB):
 
     def inputnumerics(self):  # todo rework this method
         """input the numeric data for the node"""
-        A = eval(input(
-            'How much \x1B[33m' + str(self.ingredient) + '\x1B[37m do you have on hand: '))
-        B = 0
-        while B == 0:
-            B = int(input('How much \x1B[33m' + str(self.ingredient) +
-                    '\x1B[37m do you create each time you craft it: '))
+        print('fixme dingus!')
+    def __promptint() -> int:
+        mynum : int = 0
+        while True:
+            temp = int(input(''))
+            if not temp.isdigit():
+                print('you can only type in a postive interger')
+            else:
+                mynum = temp
+                break
 
-        C = 1
-        self.amountonhand = A
-        self.amountmadepercraft = B
-        if self.parent is not None:
-            C = int(input('How much \x1B[33m' + str(self.ingredient) +
-                    '\x1B[37m do you need to create \x1B[34m' + str(self.parent.ingredient) + '\x1B[37m:'))
-            self.amountneededpercraft = C
-
-def recursivearithmetic(current: Node) -> int:
+def recursivearithmetic(cur: Node) -> int:
     """figure out the amount resulted of the augment Node instance,
     math function used: D = (B/C)A + (B/C)(min(Dqueue))
     - if there is no values in the queue it will default to 0
@@ -81,25 +77,23 @@ def recursivearithmetic(current: Node) -> int:
     """
     # check and set minimum if queue is not empty
     tentativeinterger: int = sys.maxsize
-    if len(current.queueamountresulted) == 0:
+    if len(cur.queueamountresulted) == 0:
         tentativeinterger = 0
     else:
-        for myinterger in current.queueamountresulted.items():
+        for myinterger in cur.queueamountresulted.items():
             if myinterger[1] < tentativeinterger:
                 tentativeinterger = myinterger[1]
     # tenative arithmetic code
-    # red = (current.amountmadepercraft / current.amountneeded)
-    # black = (red*current.amountonhand) + (red*tentativeinterger)
-    # black = round(math.floor(black))
-    # current.amountresulted = black
-    current.amountresulted = round(math.floor(((current.amountmadepercraft / current.amountneeded) *
-                                               current.amountonhand) + ((current.amountmadepercraft / current.amountneeded)*tentativeinterger)))  # pylint:disable=C0301
+    red = (cur.amountmadepercraft / cur.amountneeded)
+    black = (red*cur.amountonhand) + (red*tentativeinterger)
+    black = round(math.floor(black))
+    cur.amountresulted = black
     # recursively call the method
-    if current.parent is not None:
-        current.parent.queueamountresulted.update(
-            {current.ingredient: current.amountresulted})
-        recursivearithmetic(current.parent)
-    return current.amountresulted
+    if cur.parent is not None:
+        cur.parent.queueamountresulted.update(
+            {cur.ingredient: cur.amountresulted})
+        recursivearithmetic(cur.parent)
+    return cur.amountresulted
 
 def searchforendpoint(cur: Node):
     """looks for endpoint nodes to start the math method from
@@ -171,7 +165,6 @@ def populate(cur: Node):
 
 if __name__ == '__main__':
     # prompt user to type in the name of the item they want to create
-    """FIXME
     while True:
         itemname = input('What is the name of the item you want to create: ')
         itemname = itemname.strip()
@@ -180,33 +173,6 @@ if __name__ == '__main__':
         else:
             break
     head = Node(itemname, None)
-    # head.inputnumerics()
-    # populate(head)
-    """
-    focusingarray       : Node = Node('Focusing Array', None, 1, 1, 1)
-    advancedalloy       : Node = Node('Advanced Alloy', focusingarray, 8, 1, 2)
-    crystal             : Node = Node('Crystal', focusingarray, 640, 1, 2)
-    plasmiccrystal      : Node = Node('Plasmic Crystal', focusingarray, 41, 2, 2)
-    quantumprocessor    : Node = Node('Quantum Processor', focusingarray, 20, 2, 1)
-    zerchesiumbar       : Node = Node('Zerchesium Bar', advancedalloy, 548, 1, 1)
-    zerchesiumore       : Node = Node('Zerchesium Ore', zerchesiumbar, 2, 1, 2)
-    protocitebar        : Node = Node('Protocite Bar', advancedalloy, 277, 1, 1)
-    protociteore        : Node = Node('Protocite Ore', protocitebar, 2, 1, 2)
-    penumbriteshard     : Node = Node('Penumbrite Shard', advancedalloy, 86, 1, 1)
-    penumbriteore       : Node = Node('Penumbrite Ore', penumbriteshard, 2, 1, 2)
-    lead                : Node = Node('Lead', advancedalloy, 251, 1, 1)
-    crystalplantseed    : Node = Node('Crystal Plant Seed', crystal, 100, 1, 1)
-    bloodrock           : Node = Node('Blood Rock', plasmiccrystal, 1000, 1, 50)
-    blood               : Node = Node('Blood', bloodrock, 23, 1, 1)
-    lava                : Node = Node('Lava', bloodrock, 404, 1, 1)
-    protocitebarb       : Node = Node('Protocite Bar', quantumprocessor, 277, 1, 2)
-    protociteoreb       : Node = Node('Protocite Ore', protocitebarb, 2, 1, 2)
-    siliconboard        : Node = Node('Silicon Board', quantumprocessor, 310, 1, 4)
-    silicon             : Node = Node('Silicon', siliconboard, 1000, 1, 1)
-    sand                : Node = Node('Sand', silicon, 901, 1, 50)
-    copperwire          : Node = Node('Copper Wire', siliconboard, 492, 9, 1)
-    copperbar           : Node = Node('Copper Bar', copperwire, 1000, 1, 1)
-    copperore           : Node = Node('Copper Ore', copperbar, 2, 1, 2)
-    searchforendpoint(focusingarray)
-    print(focusingarray.amountresulted)
-    #print('# resulted of', head.ingredient, end=str(head.amountresulted)+'\n')
+    head.inputnumerics()
+    populate(head)
+    print('# resulted of', head.ingredient, end=str(head.amountresulted)+'\n')
---
+++ unittests/test.py
@@ -0,0 +1,44 @@
+"""unit testing code
+"""
+import unittest
+
+from main import Node, searchforendpoint
+
+
+class mytest(unittest.TestCase):
+    """_summary_
+
+    Args:
+        unittest (_type_): _description_
+    """
+    focusingarray       : Node = Node('Focusing Array', None, 1, 1, 1)
+    advancedalloy       : Node = Node('Advanced Alloy', focusingarray, 8, 1, 2)
+    crystal             : Node = Node('Crystal', focusingarray, 640, 1, 2)
+    plasmiccrystal      : Node = Node('Plasmic Crystal', focusingarray, 41, 2, 2)
+    quantumprocessor    : Node = Node('Quantum Processor', focusingarray, 20, 2, 1)
+    zerchesiumbar       : Node = Node('Zerchesium Bar', advancedalloy, 548, 1, 1)
+    zerchesiumore       : Node = Node('Zerchesium Ore', zerchesiumbar, 2, 1, 2)
+    protocitebar        : Node = Node('Protocite Bar', advancedalloy, 277, 1, 1)
+    protociteore        : Node = Node('Protocite Ore', protocitebar, 2, 1, 2)
+    penumbriteshard     : Node = Node('Penumbrite Shard', advancedalloy, 86, 1, 1)
+    penumbriteore       : Node = Node('Penumbrite Ore', penumbriteshard, 2, 1, 2)
+    lead                : Node = Node('Lead', advancedalloy, 251, 1, 1)
+    crystalplantseed    : Node = Node('Crystal Plant Seed', crystal, 100, 1, 1)
+    bloodrock           : Node = Node('Blood Rock', plasmiccrystal, 1000, 1, 50)
+    blood               : Node = Node('Blood', bloodrock, 23, 1, 1)
+    lava                : Node = Node('Lava', bloodrock, 404, 1, 1)
+    protocitebarb       : Node = Node('Protocite Bar', quantumprocessor, 277, 1, 2)
+    protociteoreb       : Node = Node('Protocite Ore', protocitebarb, 2, 1, 2)
+    siliconboard        : Node = Node('Silicon Board', quantumprocessor, 310, 1, 4)
+    silicon             : Node = Node('Silicon', siliconboard, 1000, 1, 1)
+    sand                : Node = Node('Sand', silicon, 901, 1, 50)
+    copperwire          : Node = Node('Copper Wire', siliconboard, 492, 9, 1)
+    copperbar           : Node = Node('Copper Bar', copperwire, 1000, 1, 1)
+    copperore           : Node = Node('Copper Ore', copperbar, 2, 1, 2)
+    searchforendpoint(focusingarray)
+    @classmethod
+    def test1(cls):
+        """test to see if the amount resulted of focusing array is 48
+        """
+        cls.assertTrue(cls.focusingarray.amountresulted,48)
+        
