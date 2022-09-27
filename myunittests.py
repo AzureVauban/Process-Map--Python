bc5500a64052951aa8b4f843f877de2e5a3812a7
Azurevauban <azurewight@gmail.com>
2022-09-27 17:01:32 -0500

    commit

---
+++ myunittests.py
@@ -0,0 +1,12 @@
+"""unit tesint module for Process Map (Python v1.1)
+"""
+import unittest
+
+from solution import Node
+
+
+class mytests(unittest.TestCase):
+    def test1sample(self):
+        """sample unit test
+        """
+        self.assertEqual(1, 48)
