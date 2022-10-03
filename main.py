d71b0c9b8254207da81c54eee9d1c3266a2eee7e
Azurevauban <azurewight@gmail.com>
2022-10-03 04:48:01 -0500

    commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 """
-rework this fit current development progress on V2
 - This is a reworked version of the Process Map (Python) V1, use as base for future versions
 - Mode A : The user figures out how much of item A can they make given the their supply of item
 B,C,D...
