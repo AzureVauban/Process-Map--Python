37fef785f7b91de62a4947e2f1b11258447acf77
Azurevauban <azurewight@gmail.com>
2022-10-03 18:24:21 -0500

    commit

--- unittests/test.py
+++ unittests/test.py
@@ -1,11 +1,11 @@
 """unit testing code, test out Mode A and Mode B for the script
 """
-from math import ceil
 import random
 import unittest
+from math import ceil
 
-from main import (Node, findlocalendpoints, recursivearithmetic,
-                  reversearithmetic)
+from main import reversearithmetic  # pylint:disable=E0401
+from main import Node, findlocalendpoints  # pylint:disable=E0401
 
 #To get 9999x Solar Array you need the following:
 #Tungsten Ore : 399960x #! append this output to the end:   (second generation (from head) item)
@@ -25,13 +25,29 @@ from main import (Node, findlocalendpoints, recursivearithmetic,
 #Copper Ore : 4444x
 #Gold Ore : 79992x
 
-class MODEAtesting(unittest.TestCase):
-    """Unit Testing
+def resetsupplyvalues(cur: Node):
+    """
+    resets the amount on hand attribute of all nodes into a tree to 0 for
+    use in Mode B unit testing
 
     Args:
-        unittest (_type_): test case
+        cur (Node): Node instance with information about an ingredient
     """
-    focusingarray       : Node = Node('Focusing Array', None, 1, 1, 1)
+    while cur.parent is not None:
+        cur = cur.parent
+    roots : dict = findlocalendpoints(cur,{})
+    #find endpoints
+    for endnode in roots.items():
+        endnode[1].amountonhand = 0
+        tempnode : Node = endnode[1]
+        #traverse upward and reset amount on hand 0
+        while tempnode.parent is not None:
+            tempnode.amountonhand = 0
+            tempnode = tempnode.parent
+class modebfocusingarray(unittest.TestCase):
+    """converted mode A mock tree into mode B mock tree, resets all amounts on hand of each item into 0
+    """
+    focusingarray       : Node = Node('Focusing Array', None, 0, 1, 1)
     advancedalloy       : Node = Node('Advanced Alloy', focusingarray, 8, 1, 2)
     crystal             : Node = Node('Crystal', focusingarray, 640, 1, 2)
     plasmiccrystal      : Node = Node('Plasmic Crystal', focusingarray, 41, 2, 2)
@@ -55,13 +71,12 @@ class MODEAtesting(unittest.TestCase):
     copperwire          : Node = Node('Copper Wire', siliconboard, 492, 9, 1)
     copperbar           : Node = Node('Copper Bar', copperwire, 1000, 1, 1)
     copperore           : Node = Node('Copper Ore', copperbar, 2, 1, 2)
-    tentativedict = findlocalendpoints(advancedalloy,{})
-    def test1(self):
-        """test to see if the amount resulted of focusing array is 48
+    resetsupplyvalues(sand) #reset all amounts on hand of each node to 0
+    def test2(self):
+        """_summary_
         """
-        for node in self.tentativedict.items():
-            recursivearithmetic(node[1])
-        self.assertEqual(self.focusingarray.amountresulted,48)
+        self.assertEqual(self.protociteoreb.amountonhand,0)
+        
 class MODEBtesting(unittest.TestCase):
     """unit testing class for Mode B, which is trying to figure out what should be the amount on
        hand of the endpoint items to get a desired amount of the headmost item crafted
@@ -182,14 +197,21 @@ class StickofRAMB(unittest.TestCase):
         unittest (class): Unit Test framework
     """
     #? desired amount of stick of ram is 53, resuled amount should be equal to or greater than 54
-    stickofram        : Node = Node('Stick of Ram', None, 0, 1, 1)
-    plasticpolymer    : Node = Node('Plastic Polymer', stickofram, 0, 2, 2)  # branch A
-    copper_bar : Node = Node('Copper Bar',plasticpolymer,0,4,1)
-    water : Node = Node('Water',plasticpolymer,0,4,2)
-    carbondioxide : Node = Node('Carbon Dioxide',plasticpolymer,0,4,3)
-    
-    goldbar           : Node = Node('Gold Bar', stickofram, 0, 2, 1)  # brnach B
-    siliconboard      : Node = Node('Silicon Board', stickofram, 0, 2, 2)  # branch C
+    stickofram: Node = Node('Stick of Ram', None, 0, 1, 1)
+    #ingredients for stick of ram
+    plasticpolymer: Node = Node(
+        'Plastic Polymer', stickofram, 0, 2, 2)  # branch A
+    goldbar: Node = Node('Gold Bar', stickofram, 0, 2, 1)  # branch B
+    siliconboard: Node = Node('Silicon Board', stickofram, 0, 2, 2)  # branch C
+    #ingredients for plastic polymer
+    copper_bar: Node = Node('Copper Bar', plasticpolymer, 0, 4, 1)
+    water: Node = Node('Water', plasticpolymer, 0, 4, 2)
+    carbondioxide: Node = Node('Carbon Dioxide', plasticpolymer, 0, 4, 3)
+    #ingredients for gold bar
+    goldore: Node = Node('Gold Ore', goldbar, 0, 1, 2)
+    #ingredients needed for gold ore
+    pixels: Node = Node('Pixels', goldore, 0, 1, 175)
+    #ingredients for silicon board
     assertvalue: int = reversearithmetic(stickofram, 53)
 
     def test_stickofram(self):
@@ -201,16 +223,30 @@ class StickofRAMB(unittest.TestCase):
         """amount of plastic polymer needed to craft the desired amount of stick of ram
         """
         self.assertEqual(self.plasticpolymer.amountonhand, 54)
-    def test_copperbar(self):
-        """amount of copper bar needed to craft the desired amount of plastic polymer
-        """
-        self.assertEqual(self.copper_bar.amountonhand,ceil(54/4))
+
     def test_goldbar(self):
         """amount of gold bar needed to craft the desired amount of stick of ram
         """
-        self.assertEqual(self.goldbar.amountonhand, 54/self.goldbar.amountmadepercraft)
+        self.assertEqual(self.goldbar.amountonhand, 54 /
+                         self.goldbar.amountmadepercraft)
 
     def test_siliconboard(self):
         """amount of silicon board needed to craft the desired amount of stick of ram
         """
         self.assertEqual(self.siliconboard.amountonhand, 54)
+    #* ingredients for plastic polymer
+
+    def test_copperbar(self):
+        """amount of copper bar needed to craft the desired amount of plastic polymer
+        """
+        self.assertEqual(self.copper_bar.amountonhand, ceil(54/4))
+
+    def test_water(self):
+        """amount of water needed to create the desired amount of plastic polymer
+        """
+        self.assertEqual(self.water.amountonhand, 10)
+
+    def test_carbondioxide(self):
+        """amount of carbon dioxide needed to create the desired amount of carbon dioxide
+        """
+        self.assertEqual(self.carbondioxide.amountonhand, 10)
