b48cc5a410b40c73602d8df3680d2dc0e293c61d
Azurevauban <azurewight@gmail.com>
2022-10-04 00:41:21 -0500

    commit

--- mook.py
+++ mook.py
@@ -1,18 +1,47 @@
-def test_plasmiccrystal(self):
-    """amount resulted of plasmic crystal should be 19998
+def test_quantumprocessor(self):
+    """amount resulted of quantuam processor should be 9999
     """
-    self.assertEqual(self.plasmiccrystal.amountonhand, 19998)
+    self.assertEqual(self.quantumprocessor.amountonhand, 9999)
 
 
-def test_bloodrock(self):
-    """amount resulted of blood rock should be 999900
+def test_protocitebarb(self):
+    """amount resulted of protocite bar should be 9999
     """
-    self.assertEqual(self.bloodrock.amountonhand, 999900)
-def test_blood(self):
-    """amount resulted of blood should be 999900
+    self.assertEqual(self.protocitebarb.amountonhand, 9999)
+
+
+def test_protociteoreb(self):
+    """amount resulted of protocite should be 19998
     """
-    self.assertEqual(self.blood.amountonhand, 999900)
-def test_lava(self):
-    """amount resulted of blood should be 999900
+    self.assertEqual(self.protociteoreb.amountonhand, 19998)
+
+
+def test_siliconboard(self):
+    """amount resulted of silicon board should be 19998
     """
-    self.assertEqual(self.lava.amountonhand, 999900)
+    self.assertEqual(self.siliconboard.amountonhand, 19998)
+
+
+def test_silicon(self):
+    """amount resulted of silicon should be 19998"""
+    self.assertEqual(self.silicon.amountonhand, 19998)
+
+
+def test_sand(self):
+    """amount resulted of sand 999900"""
+    self.assertEqual(self.sand.amountonhand, 19998*50)
+
+
+def test_copperwire(self):
+    """amount resulted of copper wire 19998"""
+    self.assertEqual(self.copperwire.amountonhand, 19998)
+
+
+def test_copperbar(self):
+    """amount resulted of copper bar 179982"""
+    self.assertEqual(self.copperbar.amountonhand, 19998*9)
+
+
+def test_copperore(self):
+    """amount resulted of copper ore 359964"""
+    self.assertEqual(self.copperor.amountonhand, 359964)
--- unittests/test.py
+++ unittests/test.py
@@ -67,13 +67,13 @@ class FocusingArray(unittest.TestCase):
     bloodrock           : Node = Node('Blood Rock', plasmiccrystal, 1000, 1, 50)
     blood               : Node = Node('Blood', bloodrock, 23, 1, 1)
     lava                : Node = Node('Lava', bloodrock, 404, 1, 1)
-    protocitebarb       : Node = Node('Protocite Bar', quantumprocessor, 277, 1, 2)
+    protocitebarb       : Node = Node('Protocite Bar', quantumprocessor, 277, 2, 2)
     protociteoreb       : Node = Node('Protocite Ore', protocitebarb, 2, 1, 2)
-    siliconboard        : Node = Node('Silicon Board', quantumprocessor, 310, 1, 4)
+    siliconboard        : Node = Node('Silicon Board', quantumprocessor, 310, 2, 4)
     silicon             : Node = Node('Silicon', siliconboard, 1000, 1, 1)
     sand                : Node = Node('Sand', silicon, 901, 1, 50)
-    copperwire          : Node = Node('Copper Wire', siliconboard, 492, 9, 1)
-    copperbar           : Node = Node('Copper Bar', copperwire, 1000, 1, 1)
+    copperwire          : Node = Node('Copper Wire', siliconboard, 492, 1, 1)
+    copperbar           : Node = Node('Copper Bar', copperwire, 1000, 9, 1)
     copperore           : Node = Node('Copper Ore', copperbar, 2, 1, 2)
     resetsupplyvalues(sand) #reset all amounts on hand of each node to 0
     reversearithmetic(focusingarray,9999)
@@ -156,6 +156,48 @@ class FocusingArray(unittest.TestCase):
         """amount resulted of blood should be 999900
         """
         self.assertEqual(self.lava.amountonhand, 9999*2*50)
+    #? tests for quantaum processor branch
+
+    def test_quantumprocessor(self):
+        """amount resulted of quantuam processor should be 9999
+        """
+        self.assertEqual(self.quantumprocessor.amountonhand, 9999)
+
+    def test_protocitebarb(self):
+        """amount resulted of protocite bar should be 9999
+        """
+        self.assertEqual(self.protocitebarb.amountonhand, 9999)
+
+    def test_protociteoreb(self):
+        """amount resulted of protocite should be 19998
+        """
+        self.assertEqual(self.protociteoreb.amountonhand, 19998)
+
+    def test_siliconboard(self):
+        """amount resulted of silicon board should be 19998
+        """
+        self.assertEqual(self.siliconboard.amountonhand, 19998)
+
+    def test_silicon(self):
+        """amount resulted of silicon should be 19998"""
+        self.assertEqual(self.silicon.amountonhand, 19998)
+
+    def test_sand(self):
+        """amount resulted of sand 999900"""
+        self.assertEqual(self.sand.amountonhand, 19998*50)
+
+    def test_copperwire(self):
+        """amount resulted of copper wire 19998"""
+        self.assertEqual(self.copperwire.amountonhand, 19998)
+
+    def test_copperbar(self):
+        """amount resulted of copper bar 179982"""
+        self.assertEqual(self.copperbar.amountonhand, 2222)
+
+    def test_copperore(self):
+        """amount resulted of copper ore 359964"""
+        self.assertEqual(self.copperore.amountonhand, 4444)
+
 
 class BlockOfNetherite(unittest.TestCase):
     """unit testing the recursive arithmetic method under Mode B's runtime condition
