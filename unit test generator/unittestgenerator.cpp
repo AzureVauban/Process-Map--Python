01f4391100d1ee458b1d79c6565b9d714393ef70
Azurevauban <azurewight@gmail.com>
2022-10-04 18:19:40 -0500

    commit

--- unit test generator/unittestgenerator.cpp
+++ unit test generator/unittestgenerator.cpp
@@ -5,12 +5,14 @@ the first node will always have an amount made per craft and amount needed of 1 
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+std::ifstream resultfile("generatedunittest.py"); //resulted file
 struct Node
 {
     std::string ingredient;
-    Node* parent;
-    std::vector<Node*> children;
-    Node(std::string name = "",Node* par = nullptr)
+    Node *parent;
+    std::vector<Node *> children;
+    Node(std::string name = "", Node *par = nullptr)
     {
         children = {};
         ingredient = name;
@@ -19,61 +21,89 @@ struct Node
         {
             parent->children.emplace_back(this);
         }
-    } 
+    }
     ~Node()
     {
         std::cout << "Removing " << ingredient << " from " << this << std::endl;
     }
 };
-void collectgarbage(Node* cur)
+void collectgarbage(Node *cur)
 {
     for (auto &i : cur->children)
     {
         collectgarbage(i);
     }
     delete cur;
-} 
-void populate(Node* current)
+}
+void populate(Node *current)
 {
-    //prompt if the current node has a parent, loop through its parents and output the trail
+    // prompt if the current node has a parent, loop through its parents and output the trail
     if (current->parent)
     {
-        //todo finish code here
+        // todo finish code here
     }
-    //prompt user to input nodes
+    // prompt user to input nodes
     std::vector<std::string> userinputs = {};
     std::cout << "What do you need to create " << current->ingredient << ":" << std::endl;
     while (true)
     {
         std::string myinput;
-        std::getline(std::cin,myinput);
+        std::getline(std::cin, myinput);
         if (not myinput.empty())
         {
             userinputs.emplace_back(myinput);
-        } else {
+        }
+        else
+        {
             break;
         }
     }
-    //create new nodes
+    // create new nodes
     for (const auto &str : userinputs)
     {
-        auto child = new Node(str,current);
+        auto child = new Node(str, current);
     }
-    //continue function recursively
+    // continue function recursively
     for (auto &childinstance : current->children)
     {
         populate(childinstance);
     }
 }
+void outputfile(Node *head)
+{
+    /*
+    example of a Node declared in the python unit test file
+    focusingarray       : Node = Node('Focusing Array', None, 0, 1, 1)
+    */
+    //convert any whitespace 
+    for (int i = 0; i < head->ingredient.size();i++)
+    {
+        if (head->ingredient.at(i) == ' ')
+        {
+            head->ingredient.erase(i);
+            head->ingredient.shrink_to_fit();
+        }
+    } 
+    resultfile.open("generatedunittest.py");
+    //write data to it
+    resultfile.close();
+}
 int main()
 {
+    // prompt tree title (which is the name of the mock ingredient tree)
     std::string treetitle = "";
-    do {
-    std::cout << "What is the name of the head node: ";
-    std::getline(std::cin,treetitle);
-    } while(treetitle.empty());
+    do
+    {
+        std::cout << "What is the name of the head node: ";
+        std::getline(std::cin, treetitle);
+    } while (treetitle.empty());
+    // create tree using Nodes linked to each other
     auto head = new Node(treetitle);
     populate(head);
+    // output mock unit test file
+        //write node declarations onto of the file at the top
+        //write unit test declarations onto the file below, after all declarations of been created
+    // destroy nodes and reset used memory
     collectgarbage(head);
     return 0;
 }
