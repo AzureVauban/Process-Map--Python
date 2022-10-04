6af3b69de46583f8c815399d8c6998820ad7847a
Azurevauban <azurewight@gmail.com>
2022-10-04 13:44:34 -0500

    commit

--- unit test generator/unittestgenerator.cpp
+++ unit test generator/unittestgenerator.cpp
@@ -3,18 +3,26 @@ this script is used to create ingredient mock trees for the Proces Map (Python) 
 the first node will always have an amount made per craft and amount needed of 1 and an amount on hand of 0
 */
 #include <iostream>
-#include <string>
-struct data{
-    std::string ingredient;
-    data(std::string name = "")
+#include <string> 
+#include <vector>
+struct data
+{
+    std::string ingredient; //name of the item
+    data *parent;
+    data(std::string name = "",data *P = NULL)
     {
         ingredient = name;
+        parent = P;
+    }
+    ~data()
+    {
+        std::cout << "Deleting " << ingredient << " : " << this << std::endl;
     }
 };
-void populate(data &purple); //make input trees
+void populate(data &purple); //creates trees
 int main()
 {
-    std::string headitemname = "\x1B[31mHEAD\x1B[37m";
+    std::string headitemname = "HEAD";
     data head(headitemname);
     populate(head);
     return 0;
@@ -22,4 +30,6 @@ int main()
 void populate(data &purple)
 {
     std::cout << "What ingredients do you need create " << purple.ingredient << ":" << std::endl;
+    data test("test",&purple);
+    std::cout << "end of function from" << populate << std::endl;
 }
