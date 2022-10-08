7e6b15794b70363a6502118cbfb12fa05f4bfc02
AzureVauban <haloringman@outlook.com>
2022-10-08 01:41:44 +0000

    Oct 7, 2022, 8:41 PM

--- Unit Test Generator/Node.h
+++ Unit Test Generator/Node.h
@@ -15,12 +15,14 @@ namespace NodeUtility
         /* 
         0, default mode, formats string for variable declaration and to be utilized 
         in math asseration method declaration.
-            input : advanced alloy, output : advancedalloyN
+            input: advanced alloy, output : advancedalloyN
             where N is an integer number used to help make the declaration unique in the event there are multple instances
                 the same ingredient name.
         1, formats string to be utilized in docstring of math asseration method.
-            input : advanced alloy, output : Advanced Alloy
-        2, formats string to have removed 
+            input: advanced alloy, output : Advanced Alloy
+        2, formats string to exclude leading and trailing whitespace
+            input:   advanced alloy    , output: advanced alloy
+        3, formats string to 
         */
         return red.ingredient;
     }
