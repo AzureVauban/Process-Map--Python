c5148e8b68811a0f31fbc2e937f9e165ab78fcb0
AzureVauban <haloringman@outlook.com>
2022-10-08 03:40:44 +0000

    Oct 7, 2022, 10:40 PM

--- Unit Test Generator/unittestgenerator.cpp
+++ Unit Test Generator/unittestgenerator.cpp
@@ -10,7 +10,12 @@ int main()
     // modify ofstream object
     generatedunittest << "\"\"\"Tentative Description: " << headname << std::endl
                       << "\"\"\"" << std::endl;
-     NodeUtility::create::testclass(headoflinkedlist,generatedunittest); // error on this line
+    NodeUtility::create::testclass(headoflinkedlist,generatedunittest); // error on this line
+    /* Complier Error
+    C:/msys64/mingw64/include/c++/12.1.0/fstream:853:7: note: declared here
+    853 |       basic_ofstream(const basic_ofstream&) = delete;
+        |       ^~~~~~~~~~~~~~
+    */
     generatedunittest.close();
     return 0;
 }
