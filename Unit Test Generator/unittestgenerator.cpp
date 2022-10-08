176b9f416562cc1cd4ba65919c28780bed02954b
AzureVauban <haloringman@outlook.com>
2022-10-08 03:37:24 +0000

    Oct 7, 2022, 10:37 PM

--- Unit Test Generator/unittestgenerator.cpp
+++ Unit Test Generator/unittestgenerator.cpp
@@ -2,12 +2,15 @@
 int main()
 {
     std::ofstream generatedunittest("myfile.py");
-    //prompt name of head item
+    // prompt name of head item
     std::cout << "What is the name of the item:" << std::endl;
     std::string headname = "";
-    std::getline(std::cin,headname);
-    Node head(headname);
-    //modify resulted unit test module
-    NodeUtility::create::testclass(head,generatedunittest); // error on this file
+    std::getline(std::cin, headname);
+    Node headoflinkedlist(headname);
+    // modify ofstream object
+    generatedunittest << "\"\"\"Assigned title of Head Node is " << headname << std::endl
+                      << "\"\"\"" << std::endl;
+    // NodeUtility::create::testclass(head,generatedunittest); // error on this file
+    generatedunittest.close();
     return 0;
 }
