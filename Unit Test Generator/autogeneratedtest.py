ae9a4528bb82743396c81fc67c01834990e4c3a1
AzureVauban <haloringman@outlook.com>
2022-10-08 21:48:14 +0000

    Oct 8, 2022, 4:48 PM

--- Unit Test Generator/autogeneratedtest.py
+++ Unit Test Generator/autogeneratedtest.py
@@ -1,7 +1,8 @@
 """AUTO GENERATED UNIT TEST FILE
-     USE FORMATTING FUNCTIONS OF UTILIZED IDE TO CLEANUP AND FINALIZE ANY MISTAKES REGARDING 
+    USE FORMATTING FUNCTIONS OF UTILIZED IDE TO CLEANUP AND FINALIZE ANY MISTAKES REGARDING
 FORMATTING THAT COULD BE MADE
-"""import unittest
+"""
+import unittest
 import random
 
 from main import (Node, findlocalendpoints, reversearithmetic, tentative_formatoutput)
