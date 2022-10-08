edae9ff6caab8b7d74a97023d90e38f14cdfeaf7
AzureVauban <haloringman@outlook.com>
2022-10-08 01:26:50 +0000

    Oct 7, 2022, 8:26 PM

--- Unit Test Generator/Node.h
+++ Unit Test Generator/Node.h
@@ -2,21 +2,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
-class Node
+struct Node
 {
     std::string ingredient = "";
-    public:
-    std::string name()
-    {
-        return ingredient;
-    }
 };
 namespace NodeUtility
 {
     // include functions here
     //todo make function for formatting the name of the ingredient, should have 3 modes
-    std::string parsestringformat(Node &red)
+    std::string parsestringformat(Node &red.const int )
     {
-        return red.name();
+
+        return red.ingredient;
     }
 }
