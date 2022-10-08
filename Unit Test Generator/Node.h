ab8736fa4b73a366b1b560f1ffb6e4893232e970
AzureVauban <haloringman@outlook.com>
2022-10-07 21:07:44 -0500

    issue 12

--- Unit Test Generator/Node.h
+++ Unit Test Generator/Node.h
@@ -39,27 +39,27 @@ namespace NodeUtility
     std::string parsestringformat(Node &red, format MODE = white)
     {
         /*
-        0, default mode, formats string for variable declaration and to be utilized
+        white, default mode, formats string for variable declaration and to be utilized
         in math asseration method declaration.
             input: advanced alloy, output : advancedalloyN
             where N is an integer number used to help make the declaration unique in the event there are multple instances
                 the same ingredient name.
-        1, formats string to be utilized in docstring of math asseration method.
+        purple, formats string to be utilized in docstring of math asseration method.
             input: advanced alloy, output : Advanced Alloy
-        2, formats string to exclude leading and trailing whitespace
+        green formats string to exclude leading and trailing whitespace
             input:   advanced alloy    , output: advanced alloy
-        3, formats string to
+        orange, formats string to
         */
         switch (MODE)
         {
-        case orange:
-            std::cout << "Orange" << std::endl;
+        case purple:
+            std::cout << "Purple" << std::endl;
             break;
         case green:
             std::cout << "Green" << std::endl;
             break;
-        case purple:
-            std::cout << "Purple" << std::endl;
+        case orange:
+            std::cout << "Orange" << std::endl;
             break;
         default:
             std::cout << "Default" << std::endl;
