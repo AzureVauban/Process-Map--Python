3961bd66a6e3502e26787ef6a6c9e472b99b90fe
AzureVauban <haloringman@outlook.com>
2022-10-08 22:10:44 +0000

    Oct 8, 2022, 5:10 PM

--- test.py
+++ test.py
@@ -61,7 +61,7 @@ class Issue12_single_unique_endpoint(unittest.TestCase): #pylint:disable=C0103
         totalofpixels: int = 0
         endpointnodes: dict = tentative_formatoutput(self.roots)
         floatpercents: dict = {}
-        #! totalpercent: float = 0.00 only used for 
+        #! totalpercent: float = 0.00 only used for testing, remove later
         # find total amount of an item
         for nodeinstance in endpointnodes.items():
             totalofpixels += nodeinstance[1].amountonhand
@@ -73,15 +73,16 @@ class Issue12_single_unique_endpoint(unittest.TestCase): #pylint:disable=C0103
             else:
                 raise TypeError('not an instance of', Node)
         # check to see if the total of the % rounds to 1.00
-        for floatnum in floatpercents.items():
-            totalpercent += floatnum[1]
+        #! for floatnum in floatpercents.items():
+        #!     totalpercent += floatnum[1]
+        
         # print % into debug console
         print('total # of pixels',totalofpixels,end='x\n')
         for floatnum in floatpercents.items():
             print(round(floatnum[1]*100,2),'%')
         # ? use debug console to see output
         # desired output Pixels : 147312x (2.99 % used in Lead, 17.92 % used in Irradium Ore, 24.19 % used in Trianglium Ore, 54.9 % used in Prism Shard)
-        self.assertEqual(totalpercent, 1.00)
+        self.assertEqual(len(endpointnodes), 1)
 class Issue12_multiple_unique_endpoint(unittest.TestCase): #pylint:disable=C0103
     """test new formatting and endpoint search methods from main.py module wih only one endpoint
     """
