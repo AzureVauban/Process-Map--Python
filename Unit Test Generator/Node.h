0cefcac376c091ea34b0d9a4ed056d727d45fa3b
AzureVauban <haloringman@outlook.com>
2022-10-08 05:14:25 +0000

    Oct 8, 2022, 12:14 AM

--- Unit Test Generator/Node.h
+++ Unit Test Generator/Node.h
@@ -104,12 +104,13 @@ namespace NodeUtility
             // todo create function for outputting docstring for class
             void testclass(const Node &blue, std::ofstream &pymodule)
             {
-                pymodule << "Writing Docstring for " << parsestringformat(blue) << << std::endl;
+                //pymodule << "Writing Docstring for " << parsestringformat(blue) << << std::endl;
                 // todo add code
             }
             // todo create function for outtputting docstring for method
             void method(const Node &green, std::ofstream &pymodule)
             {
+                pymodule << "Writing Docstring for " << parsestringformat(green) << " test method" << std::endl;
                 // todo add code
             }
             // todo create function for outtputting test class to output .py file
