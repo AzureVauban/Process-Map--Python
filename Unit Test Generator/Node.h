4500b231132dc3a4dfb2fd9a6bf5c9e2cd676529
AzureVauban <haloringman@outlook.com>
2022-10-08 01:22:44 +0000

    Oct 7, 2022, 8:22 PM

--- Unit Test Generator/Node.h
+++ Unit Test Generator/Node.h
@@ -2,10 +2,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
-class Node {
-std::string ingredient = "";
-};
-namespace MyNamespace
+class Node
 {
-    
+    std::string ingredient = "";
+};
+namespace NodeUtility
+{
+    // include functions here
+    //todo make function for formatting the name of the ingredient, should have 3 modes
+    void parsestringformat(Node &instance)
+    {
+        
+    }
 }
