1590b8af74351d494e207d935c03039adb6b8cbf
AzureVauban <haloringman@outlook.com>
2022-10-08 01:39:27 +0000

    Oct 7, 2022, 8:39 PM

--- Unit Test Generator/Node.h
+++ Unit Test Generator/Node.h
@@ -13,17 +13,14 @@ namespace NodeUtility
     std::string parsestringformat(Node &red, const int formattype = 0)
     {
         /* 
-        0, default mode, formats string for variable declaration
+        0, default mode, formats string for variable declaration and to be utilized 
+        in math asseration method declaration.
             input : advanced alloy, output : advancedalloyN
             where N is an integer number used to help make the declaration unique in the event there are multple instances
-                the same ingredient name
-        1, formats string to be utilized in math asseration method declaration
-            input : advanced alloy, output : advancedalloyN
-            where N is an integer number used to help make the declaration unique in the event there are multple instances
-                the same ingredient name
-        2, formats string to be utilized in docstring of math asseration method
+                the same ingredient name.
+        1, formats string to be utilized in docstring of math asseration method.
             input : advanced alloy, output : Advanced Alloy
-        3, ...
+        2, 
         */
         return red.ingredient;
     }
