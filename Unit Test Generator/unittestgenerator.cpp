07adf43447dafda58d460f27c8d0e85deb90ede3
AzureVauban <haloringman@outlook.com>
2022-10-08 01:57:16 +0000

    Oct 7, 2022, 8:57 PM

--- Unit Test Generator/unittestgenerator.cpp
+++ Unit Test Generator/unittestgenerator.cpp
@@ -1,6 +1,7 @@
 #include "Node.h"
 int main()
 {
+    std::cout << "What is the name of the item:" << std::endl;
     std::string headname = "";
     std::getline(std::cin,headname);
     Node head(headname);
