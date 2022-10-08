370278da68673dc68ae5aa2811a7f70e58128610
AzureVauban <haloringman@outlook.com>
2022-10-08 22:22:13 +0000

    Oct 8, 2022, 5:22 PM

--- test.py
+++ test.py
@@ -15,24 +15,24 @@ class Issue12_single_unique_endpoint(unittest.TestCase):  # pylint:disable=C0103
         unittest (class): Unit testing framework for python
     """
     # ? Tritanium Bar Mock Tree: https://frackinuniverse.miraheze.org/wiki/Tritanium_Bar
-    desiredamount: int = random.randint(1, 100)
+    desiredamount       : int = random.randint(1, 100)
     # it should take 6696 pixels to make one tritanium bar
-    tritaniumbar: Node = Node('Tritanium Bar')
-    lead: Node = Node('Lead', tritaniumbar)
-    pixels: Node = Node('Pixels', lead, 0, 1, 200)
-    irradiumbar: Node = Node('Irradium Bar', tritaniumbar)
-    irradiumore: Node = Node('Irradium Ore', irradiumbar, 0, 1, 2)
-    pixels2: Node = Node('Pixels', irradiumore, 0, 1, 600)
-    triangliumpyramid: Node = Node('Trianglium Pyramid', tritaniumbar)
-    triangliumore: Node = Node('Trianglium Ore', triangliumpyramid, 0, 1, 2)
-    pixels3: Node = Node('Pixels', triangliumore, 0, 1, 810)
-    prisilitestar: Node = Node('Prisilite Star', tritaniumbar)
-    prismshard: Node = Node('Prism Shard', prisilitestar, 0, 1, 2)
+    tritaniumbar        : Node = Node('Tritanium Bar')
+    lead                : Node = Node('Lead', tritaniumbar)
+    pixels              : Node = Node('Pixels', lead, 0, 1, 200)
+    irradiumbar         : Node = Node('Irradium Bar', tritaniumbar)
+    irradiumore         : Node = Node('Irradium Ore', irradiumbar, 0, 1, 2)
+    pixels2             : Node = Node('Pixels', irradiumore, 0, 1, 600)
+    triangliumpyramid   : Node = Node('Trianglium Pyramid', tritaniumbar)
+    triangliumore       : Node = Node('Trianglium Ore', triangliumpyramid, 0, 1, 2)
+    pixels3             : Node = Node('Pixels', triangliumore, 0, 1, 810)
+    prisilitestar       : Node = Node('Prisilite Star', tritaniumbar)
+    prismshard          : Node = Node('Prism Shard', prisilitestar, 0, 1, 2)
     # buy prism shards from geologist NPC ingame
-    pixels4: Node = Node('Pixels', prismshard, 0, 1, 1838)
+    pixels4             : Node = Node('Pixels', prismshard, 0, 1, 1838)
     reversearithmetic(tritaniumbar, desiredamount)
-    roots: dict = findlocalendpoints(tritaniumbar, {})
-
+    roots               : dict = findlocalendpoints(tritaniumbar, {})
+    # should 4 endpoints, but 1 (all the endpoints are pixels) item in its endpoint output
     def test_endpointsdict(self):
         """test to see if the length of the endpoints dictionary
         is 4 elements
