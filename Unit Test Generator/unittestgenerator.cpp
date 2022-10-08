ab9434c0934a3d39a1d9d8ffc2664291132a573d
AzureVauban <haloringman@outlook.com>
2022-10-08 03:00:41 +0000

    Oct 7, 2022, 10:00 PM

--- Unit Test Generator/unittestgenerator.cpp
+++ Unit Test Generator/unittestgenerator.cpp
@@ -7,5 +7,6 @@ int main()
     std::getline(std::cin,headname);
     Node head(headname);
     std::cout << "Formatted String: " << NodeUtility::parsestringformat(head) << std::endl;
+    NodeUtility::create::declaration()
     return 0;
 }
