5d9712455518b063d0230391cf3ff0f3ad64ea13
AzureVauban <haloringman@outlook.com>
2022-10-08 00:38:43 +0000

    Oct 7, 2022, 7:38 PM

--- test.py
+++ test.py
@@ -75,6 +75,6 @@ class Issue12(unittest.TestCase):
             totalpercent += floatnum[1]
         # print % into debug console
         for floatnum in floatpercents.items():
-            print(floatnum[1]*100, '%')
+            console.log(floatnum[1]*100, '%')
         # ? use debug console to see output
         self.assertEqual(totalpercent, 1.00)
