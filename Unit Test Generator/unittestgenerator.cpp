c920e402bf28c165684f53a147b5ca2ef7da0668
AzureVauban <haloringman@outlook.com>
2022-10-08 03:41:46 +0000

    Oct 7, 2022, 10:41 PM

--- Unit Test Generator/unittestgenerator.cpp
+++ Unit Test Generator/unittestgenerator.cpp
@@ -15,6 +15,11 @@ int main()
     C:/msys64/mingw64/include/c++/12.1.0/fstream:853:7: note: declared here
     853 |       basic_ofstream(const basic_ofstream&) = delete;
         |       ^~~~~~~~~~~~~~
+    
+    Node.h:97:58: note:   initializing argument 2 of 'void NodeUtility::create::testclass(const Node&, std::ofstream)'
+    97 |         void testclass(const Node &purple, std::ofstream pymodule)
+       |                                            ~~~~~~~~~~~~~~^~~~~~~~
+
     */
     generatedunittest.close();
     return 0;
