37af93b7d0002d7af33727d11f37303b628dba14
AzureVauban <haloringman@outlook.com>
2022-10-08 00:36:55 +0000

    Oct 7, 2022, 7:36 PM

--- test.py
+++ test.py
@@ -14,26 +14,27 @@ class Issue12(unittest.TestCase):
     Args:
         unittest (class): Unit testing framework for python
     """
-    tritaniumbar : Node = Node('Tritanium Bar')
-    lead : Node = Node('Lead',tritaniumbar)
-    pixels : Node = Node('Pixels',lead,0,1,200)
-    irradiumbar : Node = Node('Irradium Bar',tritaniumbar)
-    irradiumore : Node = Node('Irradium Ore',irradiumbar,0,1,2)
-    pixels2 : Node = Node('Pixels',irradiumore,0,1,600)
-    triangliumpyramid : Node = Node('Trianglium Pyramid',tritaniumbar)
-    triangliumore : Node = Node('Trianglium Ore',triangliumpyramid,0,1,2)
-    pixels3 : Node = Node('Pixels',triangliumore,0,1,810)
-    prisilitestar : Node = Node('Prisilite Star',tritaniumbar)
-    prismshard : Node = Node('Prism Shard',prisilitestar,0,1,2)
-    pixels4 : Node = Node('Pixels',prismshard,0,1,1838) #? buy from geologist NPC ingame
-    desiredamount : int = random.randint(1,100)
-    reversearithmetic(tritaniumbar,desiredamount)
-    roots : dict = findlocalendpoints(tritaniumbar,{})
+    desiredamount       : int = random.randint(1, 100)
+    tritaniumbar        : Node = Node('Tritanium Bar')
+    lead                : Node = Node('Lead', tritaniumbar)
+    pixels              : Node = Node('Pixels', lead, 0, 1, 200)
+    irradiumbar         : Node = Node('Irradium Bar', tritaniumbar)
+    irradiumore         : Node = Node('Irradium Ore', irradiumbar, 0, 1, 2)
+    pixels2             : Node = Node('Pixels', irradiumore, 0, 1, 600)
+    triangliumpyramid   : Node = Node('Trianglium Pyramid', tritaniumbar)
+    triangliumore       : Node = Node('Trianglium Ore', triangliumpyramid, 0, 1, 2)
+    pixels3             : Node = Node('Pixels', triangliumore, 0, 1, 810)
+    prisilitestar       : Node = Node('Prisilite Star', tritaniumbar)
+    prismshard          : Node = Node('Prism Shard', prisilitestar, 0, 1, 2)
+    pixels4             : Node = Node('Pixels', prismshard, 0, 1, 1838)  # buy from geologist NPC ingame
+    reversearithmetic(tritaniumbar, desiredamount)
+    roots: dict = findlocalendpoints(tritaniumbar, {})
+
     def test_endpointsdict(self):
         """test to see if the length of the endpoints dictionary
         is 4 elements
         """
-        self.assertEqual(len(self.roots),4)
+        self.assertEqual(len(self.roots), 4)
 
     def test_endpoints(self):
         """test to see if the item name of each endpoint is Pixels
@@ -47,30 +48,32 @@ class Issue12(unittest.TestCase):
                 if not iscalledpixels:
                     break
         self.assertTrue(iscalledpixels)
+
     def test_temptest(self):
         """test to see if the total sum of the endpoint node's % compostion correctly
         rounds towards 1 (100%)
         """
         tentative_formatoutput(self.roots)
         #! temp : list = list(tentative_formatoutput(self.roots).items()) save this for later, creates a list of the the instancekey and the node tuple
-        totalofpixels : int = 0
-        endpointnodes : dict = tentative_formatoutput(self.roots)
-        floatpercents : dict = {}
-        totalpercent : float = 0.00
+        totalofpixels: int = 0
+        endpointnodes: dict = tentative_formatoutput(self.roots)
+        floatpercents: dict = {}
+        totalpercent: float = 0.00
         # find total amount of an item
         for nodeinstance in endpointnodes.items():
             totalofpixels += nodeinstance[1].amountonhand
         # find the % composition of the total
         for nodeinstance in endpointnodes.items():
-            if isinstance(nodeinstance[1],Node):
-                floatpercents.update({nodeinstance[1].instancekey:nodeinstance[1].amountonhand/totalofpixels})
+            if isinstance(nodeinstance[1], Node):
+                floatpercents.update(
+                    {nodeinstance[1].instancekey: nodeinstance[1].amountonhand/totalofpixels})
             else:
-                raise TypeError('not an instance of',Node)
+                raise TypeError('not an instance of', Node)
         # check to see if the total of the % rounds to 1.00
         for floatnum in floatpercents.items():
             totalpercent += floatnum[1]
         # print % into debug console
-        for floatnum in floatpercents.items():  
-            print(floatnum[1]*100,'%')
-        #? use debug console to see output
-        self.assertEqual(totalpercent,1.00)
+        for floatnum in floatpercents.items():
+            print(floatnum[1]*100, '%')
+        # ? use debug console to see output
+        self.assertEqual(totalpercent, 1.00)
