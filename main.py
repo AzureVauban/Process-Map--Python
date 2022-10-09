41f73c49f11a68d2ccc12799145b2352c5e00eda
AzureVauban <haloringman@outlook.com>
2022-10-09 00:23:22 +0000

    Oct 8, 2022, 7:23 PM

--- main.py
+++ main.py
@@ -318,8 +318,6 @@ def tentative_formatoutput(endpoints : dict) -> dict:
     else:
         foundendpoints : dict = endpoints
     # insert elements into dict
-    black : list = list(endpoints.items())
-    white : list = black
     
     # bubble check for duplicate node names, make a copy of a dict as a list
     # if a node has the same name, remove the b node and amount resulted/amount on hand (dependent on mode) to the a node
