34c861c008667f603ca383fae021d4cbb866a87c
AzureVauban <haloringman@outlook.com>
2022-10-11 19:23:54 +0000

    Oct 11, 2022, 2:23 PM

--- Unit Test Generator/generatedUnitTest.py
+++ Unit Test Generator/generatedUnitTest.py
@@ -4,24 +4,24 @@ import unittest
 
 from main import Node
 
-Block_of_Netherite: Node = Node('Block of Netherite', None, 0, 1, 1)
-morphite: Node = Node('morphite', Block_of_Netherite, 0, 1, 5)
+Block_of_Emeralds: Node = Node('Block of Emeralds', None, 0, 1, 1)
+Emerald: Node = Node('Emerald', Block_of_Emeralds, 0, 1, 9)
 
 
-class BlockofNetheritete_unittest(unittest.TestCase): # pylint:disable=C0103
+class BlockofEmeraldsds_unittest(unittest.TestCase): # pylint:disable=C0103
 	"""tentative test class, add additional comments here:
 	"""
 
-	def test_Block_of_Netherite(self):# pylint:disable=C0103
-		"""assert that Block of Netherite is equal to 5
+	def test_Block_of_Emeralds(self):# pylint:disable=C0103
+		"""assert that Block of Emeralds is equal to 64
 		"""
 
-		self.assertEqual(Block_of_Netherite.amountonhand,5)
+		self.assertEqual(Block_of_Emeralds.amountonhand,64)
 
-	def test_morphite(self):# pylint:disable=C0103
-		"""assert that morphite is equal to 25
+	def test_Emerald(self):# pylint:disable=C0103
+		"""assert that Emerald is equal to 576
 		"""
 
-		self.assertEqual(morphite.amountonhand,25)
+		self.assertEqual(Emerald.amountonhand,576)
 
 
