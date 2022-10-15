fa5dec0441ec26a71820ffdce1f5602159ab1685
AzureVauban <haloringman@outlook.com>
2022-10-15 23:44:58 +0000

    Oct 15, 2022, 6:44 PM

--- main.py
+++ main.py
@@ -374,6 +374,9 @@ def tentative_formatoutput(endpoints: dict) -> SplitEndpoints:
     blue_blue: dict = {}
     for node in endpoints.items():
         if node[1].ingredient not in red_dict:
+            blue_dict.update({node[1].ingredient: node[1].amountonhand})
+        else:
+            blue_dict[node[1].ingredient] += node[1].amountonhand
         
     return SplitEndpoints(red_dict, blue_blue)
 
