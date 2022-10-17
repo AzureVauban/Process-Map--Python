5d25be314e4a9a69462ec9ab5514c967e4d0635a
AzureVauban <haloringman@outlook.com>
2022-10-17 07:41:15 +0000

    Oct 17, 2022, 2:41 AM

--- Unit Test Generator/auto_generated_unittest.py
+++ Unit Test Generator/auto_generated_unittest.py
@@ -4,45 +4,31 @@ import unittest
 
 from main import Node, reversearithmetic
 
-Block_of_Emerald: Node = Node('Block of Emerald', None, 0, 1, 1)
-Emerald: Node = Node('Emerald', Block_of_Emerald, 0, 1, 9)
-Stick: Node = Node('Stick', Emerald, 0, 1, 32)
-Oak_Planks: Node = Node('Oak Planks', Stick, 0, 4, 2)
-Oak_Wood: Node = Node('Oak Wood', Oak_Planks, 0, 4, 1)
-reversearithmetic(Block_of_Emerald, 1)
+Gold_Bar: Node = Node('Gold Bar', None, 0, 1, 1)
+Gold_Ore_: Node = Node('Gold Ore ', Gold_Bar, 0, 1, 2)
+Pixels: Node = Node('Pixels', Gold_Ore_, 0, 1, 200)
+reversearithmetic(Gold_Bar, 1)
 
 
-class BLOCKOFEMERALDLD_unittest(unittest.TestCase): # pylint:disable=C0103
+class GOLDBARR_unittest(unittest.TestCase): # pylint:disable=C0103
 	"""tentative test class, add additional comments here:
 	"""
 
-	def test_Block_of_Emerald(self): # pylint:disable=C0103
-		"""assert that Block of Emerald is equal to 1
+	def test_Gold_Bar(self): # pylint:disable=C0103
+		"""assert that Gold Bar is equal to 1
 		"""
 
-		self.assertEqual(Block_of_Emerald.amountonhand,1)
+		self.assertEqual(Gold_Bar.amountonhand,1)
 
-	def test_Emerald(self): # pylint:disable=C0103
-		"""assert that Emerald is equal to 9
+	def test_Gold_Ore_(self): # pylint:disable=C0103
+		"""assert that Gold Ore  is equal to 2
 		"""
 
-		self.assertEqual(Emerald.amountonhand,9)
+		self.assertEqual(Gold_Ore_.amountonhand,2)
 
-	def test_Stick(self): # pylint:disable=C0103
-		"""assert that Stick is equal to 288
+	def test_Pixels(self): # pylint:disable=C0103
+		"""assert that Pixels is equal to 400
 		"""
 
-		self.assertEqual(Stick.amountonhand,288)
-
-	def test_Oak_Planks(self): # pylint:disable=C0103
-		"""assert that Oak Planks is equal to 144
-		"""
-
-		self.assertEqual(Oak_Planks.amountonhand,144)
-
-	def test_Oak_Wood(self): # pylint:disable=C0103
-		"""assert that Oak Wood is equal to 36
-		"""
-
-		self.assertEqual(Oak_Wood.amountonhand,36)
+		self.assertEqual(Pixels.amountonhand,400)
 
